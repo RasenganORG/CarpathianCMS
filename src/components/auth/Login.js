fd8410e8dc73c24a6ffbbdc9a2793b5263fc2f6b
Edward484 <selea.eduard@gmail.com>
2022-06-30 17:31:02 +0300

    added ant icons done login and signup pages only visually worked on settings page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,13 +1,105 @@
-import { Button } from 'antd';
+import { Button, Card, Col, Form, Image, Input, Row, Space, Typography } from 'antd';
+import {useNavigate} from 'react-router-dom'
 import 'antd/dist/antd.css';
 
 const Login = () => {
+  const navigate = useNavigate()
 
-  return(
-    <Button>
-      Login
-    </Button>
+  function onFinishForm(data) {
+    console.log(data);
+    navigate('/settings')
+  }
+
+  return (
+    <>
+      <Row>
+        <Col span={3}>
+          <div
+            style={{
+              height: '250px',
+            }}
+          />
+        </Col>
+        <Col offset={14}>
+          <Typography.Title level={4} style={{
+            color:'darkblue'
+          }}>
+            New here?
+          </Typography.Title>
+          <Button
+            type={'primary'}
+            onClick={() => {
+              navigate('/auth/register')
+            }}
+          >
+            <Typography>
+              Register Now
+            </Typography>
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={8} offset={1}>
+          <img
+            width={500}
+            src='https://img.freepik.com/free-vector/forgot-password-concept-illustration_114360-1123.jpg?t=st=1656595571~exp=1656596171~hmac=63e63dfae8f62d8b6a1eb7eae6261c2c72784d9b1dce928137478635019b12f2&w=740'
+          />
+        </Col>
+        <Col span={8} offset={4}>
+          <Form
+            onFinish={onFinishForm}
+            style={{
+              width: '400px',
+            }}
+          >
+            <Typography.Title level={5}>
+              Email
+            </Typography.Title>
+            <Form.Item name={'email'}>
+              <Input
+                placeholder={'Enter email'}
+                required
+                allowClear
+                style={{
+                  width: '100%',
+                  height: '50px',
+                }}
+              ></Input>
+            </Form.Item>
+            <Typography.Title level={5}>
+              Password
+            </Typography.Title>
+            <Form.Item name={'password'}>
+              <Input.Password
+                placeholder={'Enter Password'}
+                required
+                style={{
+                  width: '100%',
+                  height: '50px',
+                }}
+              >
+              </Input.Password>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                block
+                type={'primary'}
+                htmlType={'submit'}
+                style={{
+                  backgroundColor: 'aliceblue',
+                  color: 'black',
+                }}
+                size={'large'}
+              >
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </>
   )
-}
+    ;
+};
 
 export default Login;
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,13 +1,136 @@
-import { Button } from 'antd';
+import { Button, Col, Form,  Input, Row, Typography } from 'antd';
+import {useNavigate} from 'react-router-dom'
 import 'antd/dist/antd.css';
 
 const Signup = () => {
+  const navigate = useNavigate()
 
-  return(
-    <Button>
-      Signup
-    </Button>
+  function onFinishForm(data) {
+    console.log(data);
+    navigate('/settings')
+  }
+
+  return (
+    <>
+      <Row>
+        <Col span={3}>
+          <div
+            style={{
+              height: '250px',
+            }}
+          />
+        </Col>
+        <Col offset={14}>
+          <Typography.Title level={4} style={{
+            color:'darkblue'
+          }}>
+            Already have an account?
+          </Typography.Title>
+          <Button
+            type={'primary'}
+            onClick={() => {
+              navigate('/auth/login')
+            }}
+          >
+            <Typography>
+              Login Now
+            </Typography>
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={8} offset={1}>
+          <img
+            width={500}
+            src='https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-135.jpg?t=st=1656595571~exp=1656596171~hmac=f6abf5832abaff87deb0d8729cf1cf54c28cc73f638eae2069dc5fbe882e2edb&w=740'
+          />
+        </Col>
+        <Col span={8} offset={4}>
+          <Form
+            onFinish={onFinishForm}
+            style={{
+              width: '400px',
+            }}
+          >
+            <Typography.Title level={5}>
+              Email
+            </Typography.Title>
+            <Form.Item name={'email'}>
+              <Input
+                placeholder={'Enter email'}
+                required
+                allowClear
+                style={{
+                  width: '100%',
+                  height: '50px',
+                }}
+              ></Input>
+            </Form.Item>
+
+            <Typography.Title level={5}>
+              First Name
+            </Typography.Title>
+            <Form.Item name={'firstName'}>
+              <Input
+                placeholder={'First Name'}
+                required
+                allowClear
+                style={{
+                  width: '100%',
+                  height: '50px',
+                }}
+              ></Input>
+            </Form.Item>
+
+            <Typography.Title level={5}>
+              Last Name
+            </Typography.Title>
+            <Form.Item name={'lastName'}>
+              <Input
+                placeholder={'Last Name'}
+                required
+                allowClear
+                style={{
+                  width: '100%',
+                  height: '50px',
+                }}
+              ></Input>
+            </Form.Item>
+
+            <Typography.Title level={5}>
+              Password
+            </Typography.Title>
+            <Form.Item name={'password'}>
+              <Input.Password
+                placeholder={'Enter Password'}
+                required
+                style={{
+                  width: '100%',
+                  height: '50px',
+                }}
+              >
+              </Input.Password>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                block
+                type={'primary'}
+                htmlType={'submit'}
+                style={{
+                  backgroundColor: 'aliceblue',
+                  color: 'black',
+                }}
+                size={'large'}
+              >
+                Sign up
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </>
   )
-}
+    ;
+};
 
 export default Signup;
